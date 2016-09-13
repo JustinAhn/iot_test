5f4793cdfa806e19804dba7ec34106cc754a55a5
it <it@DESKTOP-KKAQ0RK>
2016-09-13 14:57:04 +0900

    dd

---
+++ sum1/src/sum1/sum1.java
@@ -0,0 +1,14 @@
+package sum1;
+
+public class sum1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int v1 = 1;
+		 int v2 = 2; 
+		 int result = v1+v2;
+		 
+		 System.out.println("v1과v2 더하기 값은?"+result+"입니다.");
+	}	
+	
+	}
